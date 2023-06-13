d27390ec05d0bac9729f3438cc8f90f9c2ebbd70
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-06-13 21:22:41 +0300

    firstAnimation

---
+++ RandomAnimation/ViewControllers/RandAnimationViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  RandomAnimation
+//
+//  Created by Роман on 13.06.2023.
+//
+
+import UIKit
+import SpringAnimation
+
+final class RandAnimationViewController: UIViewController {
+
+    @IBOutlet var viewAnimation: SpringView!
+    @IBOutlet var runButtonOutlet: SpringButton!
+
+    @IBOutlet var delayNumberLabel: UILabel!
+    @IBOutlet var forceNumberLabel: UILabel!
+    @IBOutlet var curveNumderLabel: UILabel!
+    @IBOutlet var animationNameLabel: UILabel!
+    
+    @IBAction func runButtonStart(_ sender: SpringButton) {
+        viewAnimation.animation = "shake"
+        viewAnimation.curve = "easeIn"
+        viewAnimation.force = 2
+        viewAnimation.duration = 1
+        viewAnimation.delay = 0.3
+        viewAnimation.animate()
+        
+        sender.animation = "shake"
+        sender.curve = "easeIn"
+        sender.force = 2
+        sender.duration = 1
+        sender.delay = 0.3
+        sender.animate()
+        
+    }
+    
+}
+
