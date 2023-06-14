b8ff4d78ad7bf3bb14f784bdc0334a85ad724661
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-06-14 15:33:51 +0300

    Final edite

--- RandomAnimation/ViewControllers/RandAnimationViewController.swift
+++ RandomAnimation/ViewControllers/RandAnimationViewController.swift
@@ -18,29 +18,39 @@ final class RandAnimationViewController: UIViewController {
     private var delayAnimation = 0.0
     private var durationAnimation = 0
     
+    @IBOutlet var labaleInfo: SpringView!
     @IBOutlet var viewAnimation: SpringView!
     @IBOutlet var runButtonOutlet: SpringButton!
 
+    @IBOutlet var durationNumberLabel: UILabel!
     @IBOutlet var delayNumberLabel: UILabel!
     @IBOutlet var forceNumberLabel: UILabel!
     @IBOutlet var curveNumderLabel: UILabel!
     @IBOutlet var animationNameLabel: UILabel!
     
     @IBAction func runButtonStart(_ sender: SpringButton) {
-        getRandomValueAnimation()
+        if nameAnimation != "" {
+            sender.backgroundColor = .purple
+            
+        }
+        labaleInfo.animation = "wobble"
+        labaleInfo.force = 1
+        labaleInfo.delay = 0
+        labaleInfo.curve = "easeIn"
+        labaleInfo.duration = 0.7
+        labaleInfo.animate()
+        
         setValueTitle()
         
+        viewAnimation.animation = nameAnimation
+        viewAnimation.curve = curveAnimation
         viewAnimation.force = CGFloat(forceAnimation)
         viewAnimation.duration = CGFloat(durationAnimation)
         viewAnimation.delay = CGFloat(delayAnimation)
         viewAnimation.animate()
         
-        sender.animation = "shake"
-        sender.curve = "easeIn"
-        sender.force = 2
-        sender.duration = 1
-        sender.delay = 0.3
-        sender.animate()
+        getRandomValueAnimation()
+        runButtonOutlet.setTitle("Start: \(nameAnimation)", for: .normal)
         
     }
     
@@ -49,15 +59,9 @@ final class RandAnimationViewController: UIViewController {
          
         nameAnimation = anination?.nameAnimation ?? ""
         curveAnimation = anination?.curveAnimation ?? ""
-        
-        viewAnimation.animation = nameAnimation
-        viewAnimation.curve = curveAnimation
-        
         forceAnimation = Int.random(in: 1...5)
-        
-        delayAnimation = Double(String(format: "%.1f", Double.random(in: 0...1))) ?? 0.0
-        
-        durationAnimation = Int.random(in: 1...6)
+        delayAnimation = Double(String(format: "%.1f", Double.random(in: 0...5))) ?? 0.0
+        durationAnimation = Int.random(in: 1...5)
         
     }
     
@@ -66,6 +70,7 @@ final class RandAnimationViewController: UIViewController {
         curveNumderLabel.text = "Curve: \(curveAnimation)"
         forceNumberLabel.text = "Force: \(forceAnimation)"
         delayNumberLabel.text = "Delay: \(delayAnimation)"
+        durationNumberLabel.text = "Duration: \(durationAnimation)"
     }
 }
 
